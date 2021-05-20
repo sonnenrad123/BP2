20e4ba4f38afb82f79e4f4ba98a8653208f2e1ad
Ivan Gajic <33852380+sonnenrad123@users.noreply.github.com>
2021-05-20 19:59:17 +0200

    Added CRUD interfaces

---
+++ MusicCompetitionBP2/Common/Interfaces/IAllOperations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Interfaces
+{
+    [ServiceContract]
+    public interface IAllOperations:ICreateOperations,IReadOperations,IUpdateOperations,IDeleteOperations
+    {
+        [OperationContract]
+        bool AddCompetitorToCompetition(long competitorJMBG, int competitionID);
+        [OperationContract]
+        bool AddEvaluationForMusicPerformance(long juryMemberJMBG, int genreID, int performanceID, short mark, string comment);
+        [OperationContract]
+        bool AddGenreExpertise(int genreID, long juryMemberJMBG);
+        [OperationContract]
+        bool HireSingerForCompetition(long juryMemberJMBG, int competitionID);
+        [OperationContract]
+        bool AddPublishingHouseOrganization(int competitionID, int phID);
+        [OperationContract]
+        bool AddHallReservation(int competitionID, int phID, int hallID, DateTime dATE_RES, TimeSpan sTART_TIME, TimeSpan eND_TIME);
+    }
+}
---
+++ MusicCompetitionBP2/Common/Interfaces/IUpdateOperations.cs
@@ -0,0 +1,31 @@
+﻿using Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Interfaces
+{
+    [ServiceContract]
+    public interface IUpdateOperations
+    {
+        [OperationContract]
+        void EditSinger(Singer s);
+        [OperationContract]
+        void EditCompetitor(Competitor c);
+        [OperationContract]
+        void EditJuryMember(JuryMember jM);
+        [OperationContract]
+        void EditMusicPerformance(MusicPerformance mP);
+        [OperationContract]
+        void EditGenre(Genre g);
+        [OperationContract]
+        void EditCompetition(Competition c);
+        [OperationContract]
+        void EditPublishingHouse(PublishingHouse ph);
+        [OperationContract]
+        void EditPerformanceHall(PerformanceHall pH);
+    }
+}
